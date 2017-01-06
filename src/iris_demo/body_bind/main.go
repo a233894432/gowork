436302a228e147578af8838707fdcf408ab22b99
xiangxiucheng <xiangxiucheng@ttyun.com>
2017-01-06 16:25:09 +0800

    更新 readme.md

---
+++ src/iris_demo/body_bind/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/kataras/iris"
+)
+
+type Visitor struct {
+	Username string
+	Mail     string
+	Data     []string `form:"mydata"`
+}
+
+func main() {
+
+	iris.Get("/", func(ctx *iris.Context) {
+		ctx.Render("form.html", nil)
+		ctx.Log(ctx.RequestIP())
+
+	})
+
+	iris.Post("/form_action", func(ctx *iris.Context) {
+		visitor := Visitor{}
+		err := ctx.ReadForm(&visitor)
+		if err != nil {
+			fmt.Println("Error when reading form: " + err.Error())
+		}
+		fmt.Printf("\n Visitor: %v", visitor)
+		// ctx.RedirectTo("/")
+	})
+
+	iris.Listen(":8080")
+}
